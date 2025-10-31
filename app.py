be156577326d8e022d1425a547182bbfef3b66c4
mr-gorsky <ktcn.research@gmail.com>
2025-10-31 22:18:13 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Maritime Vision Test")
+st.title("🚢 Maritime Color Vision Test")
+st.success("✅ FINALLY WORKING!")
+st.write("Basic version - we'll add tests step by step")
